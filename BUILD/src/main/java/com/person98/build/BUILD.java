263bbd56109f4315ff60935bfade200f50d1df02
Person98 <jacesleeman603@gmail.com>
2023-12-14 06:06:14 -0500

    Init 1.20.2

---
+++ BUILD/src/main/java/com/person98/build/BUILD.java
@@ -0,0 +1,17 @@
+package com.person98.build;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BUILD extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ MAIN/src/main/java/com/person98/main/MAIN.java
@@ -0,0 +1,17 @@
+package com.person98.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MAIN extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
